f99cc3c5705df737dabb65329cc467453c464e32
Will Lee <will.i.lee@outlook.com>
2018-06-11 22:25:18 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,8 +3,8 @@
 
 def key_for_min_value(name_hash)
   return nil if name_hash == {}
-  min_key = 
-  min_value = 
+  min_key = name_hash.to_a[0,0]
+  min_value =
   name_hash.each do |item, value|
     min_key = item if value < min_value
   end
